b4562b9ee209b0e5ceb0478aea575b4fe6ee97c3
YouJinTou <dandreevd@gmail.com>
2020-10-20 20:36:28 +0300

    Added an API.

---
+++ src/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/api/v1/healthcheck", healthcheck)
+	router.Run()
+}
+
+func healthcheck(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status": 200,
+	})
+}
