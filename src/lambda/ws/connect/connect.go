8b036b3479023ff1e76dfd498a8a11c626d3322c
YouJinTou <dandreevd@gmail.com>
2020-10-21 22:52:00 +0300

    All reasonable attempts to use the golang AWS SDK have failed. Switching to Python.

--- src/lambda/ws/connect/connect.go
+++ src/lambda/ws/connect/connect.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
@@ -17,8 +19,9 @@ type Item struct {
 	Timestamp    int64
 }
 
-func handle(_ context.Context, req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
-	dynamo := dynamodb.New(session.New())
+func handle(ctx context.Context, req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	session := session.Must(session.NewSession())
+	dynamo := dynamodb.New(session)
 	item := Item{
 		ConnectionID: req.RequestContext.ConnectionID,
 		Timestamp:    req.RequestContext.ConnectedAt,
@@ -37,6 +40,23 @@ func handle(_ context.Context, req events.APIGatewayWebsocketProxyRequest) (even
 
 	if putError != nil {
 		return events.APIGatewayProxyResponse{StatusCode: 500}, putError
+	}
+
+	endpoint := fmt.Sprintf(
+		"https://%s.execute-api.%s.amazonaws.com/%s",
+		req.RequestContext.DomainName,
+		"eu-central-1",
+		req.RequestContext.Stage)
+	apiClient := apigatewaymanagementapi.New(session, aws.NewConfig().WithEndpoint(endpoint))
+	connectionInput := apigatewaymanagementapi.PostToConnectionInput{
+		ConnectionId: aws.String(req.RequestContext.ConnectionID),
+		Data:         []byte("{ poolID: \"123\" }"),
+	}
+	request, _ := apiClient.PostToConnectionRequest(&connectionInput)
+	postError := request.Send()
+
+	if postError != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 500}, postError
 	}
 
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
