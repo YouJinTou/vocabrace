21316e6cbbc3708356dc7b785f3d74aaa1c27aaf
YouJinTou <dandreevd@gmail.com>
2020-10-24 00:42:55 +0300

    Implemented pool.JoinOrCreate().

--- src/lambda/ws/connect/connect.go
+++ src/lambda/ws/connect/connect.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 
+	pool "github.com/YouJinTou/vocabrace/pooling"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 // Item The DynamoDB item for the 'connections' table
@@ -20,44 +15,27 @@ type Item struct {
 }
 
 func handle(ctx context.Context, req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
-	session := session.Must(session.NewSession())
-	dynamo := dynamodb.New(session)
-	item := Item{
+	pool.JoinOrCreate(&pool.Request{
 		ConnectionID: req.RequestContext.ConnectionID,
-		Timestamp:    req.RequestContext.ConnectedAt,
-	}
-	marshalled, err := dynamodbattribute.MarshalMap(item)
+		UserID:       "123",
+		PoolLimit:    5})
+	// endpoint := fmt.Sprintf(
+	// 	"https://%s.execute-api.%s.amazonaws.com/%s",
+	// 	req.RequestContext.DomainName,
+	// 	"eu-central-1",
+	// 	req.RequestContext.Stage)
+	// session := session.Must(session.NewSession(&aws.Config{Region: aws.String("eu-central-1")}))
+	// apiClient := apigatewaymanagementapi.New(session, aws.NewConfig().WithEndpoint(endpoint))
+	// connectionInput := apigatewaymanagementapi.PostToConnectionInput{
+	// 	ConnectionId: aws.String(req.RequestContext.ConnectionID),
+	// 	Data:         []byte("{ poolID: \"123\" }"),
+	// }
+	// request, _ := apiClient.PostToConnectionRequest(&connectionInput)
+	// postError := request.Send()
 
-	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 500}, err
-	}
-
-	input := dynamodb.PutItemInput{
-		Item:      marshalled,
-		TableName: aws.String("connections"),
-	}
-	_, putError := dynamo.PutItem(&input)
-
-	if putError != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 500}, putError
-	}
-
-	endpoint := fmt.Sprintf(
-		"https://%s.execute-api.%s.amazonaws.com/%s",
-		req.RequestContext.DomainName,
-		"eu-central-1",
-		req.RequestContext.Stage)
-	apiClient := apigatewaymanagementapi.New(session, aws.NewConfig().WithEndpoint(endpoint))
-	connectionInput := apigatewaymanagementapi.PostToConnectionInput{
-		ConnectionId: aws.String(req.RequestContext.ConnectionID),
-		Data:         []byte("{ poolID: \"123\" }"),
-	}
-	request, _ := apiClient.PostToConnectionRequest(&connectionInput)
-	postError := request.Send()
-
-	if postError != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 500}, postError
-	}
+	// if postError != nil {
+	// 	return events.APIGatewayProxyResponse{StatusCode: 500}, postError
+	// }
 
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
 }
