ab33c9b10eed18a2c16a8bcf3f0a0586c029be1d
YouJinTou <dandreevd@gmail.com>
2020-10-24 14:19:49 +0300

    Implemented pool.Leave().

--- src/lambda/ws/disconnect/disconnect.go
+++ src/lambda/ws/disconnect/disconnect.go
@@ -3,32 +3,21 @@ package main
 import (
 	"context"
 
+	pool "github.com/YouJinTou/vocabrace/pooling"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
+func main() {
+	lambda.Start(handle)
+}
+
 func handle(_ context.Context, req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
-	dynamo := dynamodb.New(session.New())
-	input := dynamodb.DeleteItemInput{
-		Key: map[string]*dynamodb.AttributeValue{
-			"ConnectionID": {
-				S: aws.String(req.RequestContext.ConnectionID),
-			},
-		},
-		TableName: aws.String("connections"),
-	}
-	_, err := dynamo.DeleteItem(&input)
+	err := pool.Leave(req.RequestContext.ConnectionID)
 
 	if err != nil {
 		return events.APIGatewayProxyResponse{StatusCode: 500}, err
 	}
 
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
-}
-
-func main() {
-	lambda.Start(handle)
 }
