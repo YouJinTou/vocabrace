2385b51a71f28e44dd6bc2974d417cdeef8af476
YouJinTou <dandreevd@gmail.com>
2020-10-25 21:43:12 +0200

    Fixed function name.

--- src/lambda/ws/disconnect/build.sh
+++ src/lambda/ws/disconnect/build.sh
@@ -1,7 +1,7 @@
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o disconnect disconnect.go
 build-lambda-zip -output disconnect.zip disconnect ../config.prod.json
 aws lambda update-function-code \
-    --function-name ondisconnect \
+    --function-name onDisconnect \
     --zip-file fileb://disconnect.zip
 rm disconnect
 rm disconnect.zip
--- src/lambda/ws/publish/build.sh
+++ src/lambda/ws/publish/build.sh
@@ -1,7 +1,7 @@
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o publish publish.go
 build-lambda-zip -output publish.zip publish ../config.prod.json
 aws lambda update-function-code \
-    --function-name onpublish \
+    --function-name onPublish \
     --zip-file fileb://publish.zip
 rm publish
 rm publish.zip
