faec57cfaabf9d0ad16274ff5b11f4a8da8e88ed
YouJinTou <dandreevd@gmail.com>
2020-10-25 10:22:59 +0200

    Added memcached/client.go.

--- src/go.mod
+++ src/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/aws/aws-lambda-go v1.19.1
 	github.com/aws/aws-sdk-go v1.35.14
+	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/google/uuid v1.1.2
 )
---
+++ src/memcached/client.go
@@ -0,0 +1,105 @@
+package memcached
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/bradfitz/gomemcache/memcache"
+)
+
+// Client wraps the bradfitz's memcache.Client.
+type Client struct {
+	bfc *memcache.Client
+}
+
+// New creates a new client.
+func New(server string) *Client {
+	return &Client{
+		bfc: memcache.New(server),
+	}
+}
+
+// Get sets a key. Will retry 3 times.
+func (c Client) Get(key string) (*memcache.Item, error) {
+	var err error = nil
+
+	for i := 0; i < 3; i++ {
+		item, getErr := c.bfc.Get(key)
+
+		if getErr == nil {
+			return item, nil
+		} else {
+			err = getErr
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	fmt.Println(err)
+
+	return nil, err
+}
+
+// Set sets a key. Will retry 3 times.
+func (c Client) Set(item *memcache.Item) error {
+	var err error = nil
+
+	for i := 0; i < 3; i++ {
+		err = c.bfc.Set(item)
+
+		if err == nil {
+			return err
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return err
+}
+
+// ListAppend updates a key whose value is a list.
+func (c Client) ListAppend(key, toAdd string) error {
+	var err error = nil
+
+	for i := 0; i < 1000; i++ {
+		item, getErr := c.bfc.Get(key)
+
+		if getErr != nil {
+			err = getErr
+
+			fmt.Println(fmt.Sprintf("Get miss: %s (lookup key), %s (addable).", key, toAdd))
+
+			continue
+		}
+
+		var oldItems []string
+
+		json.Unmarshal(item.Value, &oldItems)
+
+		newItems := append(oldItems, toAdd)
+		newItemsMarshalled, _ := json.Marshal(newItems)
+		item.Value = newItemsMarshalled
+		casErr := c.bfc.CompareAndSwap(item)
+
+		if casErr == nil {
+			err = nil
+
+			break
+		} else {
+			err = casErr
+		}
+	}
+
+	if err == nil {
+		fmt.Println(fmt.Sprintf("Appended %s to %s", toAdd, key))
+	} else {
+		fmt.Println(fmt.Sprintf("Failed to CAS %s/%s", toAdd, key))
+	}
+
+	return err
+}
