7d1acfc41fde1286e83c688e42402440c579f368
YouJinTou <dandreevd@gmail.com>
2020-11-10 21:20:16 +0200

    Added better error handling.

--- src/ws/scrabble.go
+++ src/ws/scrabble.go
@@ -2,12 +2,29 @@ package ws
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/YouJinTou/vocabrace/games/scrabble"
 )
 
 type scrabblews struct{}
+type turn struct {
+	IsPlace       bool
+	IsExchange    bool
+	IsPass        bool
+	Word          string
+	ExchangeTiles []string
+}
+type result struct {
+	g   *scrabble.Game
+	m   string
+	err error
+}
+type clientMessage struct {
+	Body string
+	Type string
+}
 
 func (s scrabblews) OnStart(data *ReceiverData) {
 	type start struct {
@@ -40,37 +57,30 @@ func (s scrabblews) OnStart(data *ReceiverData) {
 }
 
 func (s scrabblews) OnAction(data *ReceiverData) {
-	type turn struct {
-		IsPlace       bool
-		IsExchange    bool
-		IsPass        bool
-		Word          string
-		ExchangeTiles []string
-	}
-	current := turn{}
-	err := json.Unmarshal([]byte(data.Body), &current)
+	turn := turn{}
+	bErr := json.Unmarshal([]byte(data.Body), &turn)
 
-	if err != nil {
-		fmt.Println(fmt.Sprintf("Data: %s Dump: %s", data, err.Error()))
+	if bErr != nil {
+		s.sendError(data, "Invalid payload.", bErr)
 		return
 	}
 
 	game := &scrabble.Game{}
+	var r *result
 	loadState(data, game)
 
-	if current.IsExchange {
-		g, err := game.Exchange(current.ExchangeTiles)
-		if err != nil {
-			fmt.Println(fmt.Sprintf("Tile exchange. Data: %s Dump: %s", data, err.Error()))
-			return
-		}
-		game = &g
-	} else if current.IsPass {
+	if turn.IsExchange {
+		r = s.exchange(&turn, game)
+	} else if turn.IsPass {
 
-	} else if current.IsPlace {
+	} else if turn.IsPlace {
 
 	} else {
-		fmt.Println(fmt.Sprintf("Invalid action. Data: %s Dump: %s", data, err.Error()))
+		r = &result{nil, "", errors.New("invalid action")}
+	}
+
+	if r.err != nil {
+		s.sendError(data, "Invalid action.", r.err)
 		return
 	}
 
@@ -81,8 +91,16 @@ func (s scrabblews) OnAction(data *ReceiverData) {
 	SendMany(data.otherConnections(), Message{
 		Domain:  data.Domain,
 		Stage:   data.Stage,
-		Message: "Player placed word!",
+		Message: r.m,
 	})
+}
+
+func (s *scrabblews) exchange(turn *turn, g *scrabble.Game) *result {
+	game, err := g.Exchange(turn.ExchangeTiles)
+	if err != nil {
+		return &result{&game, "EXCHANGE FAILED", err}
+	}
+	return &result{&game, "EXCHANGED", err}
 }
 
 func (s *scrabblews) loadPlayers(connectionIDs []string) []*scrabble.Player {
@@ -95,4 +113,18 @@ func (s *scrabblews) loadPlayers(connectionIDs []string) []*scrabble.Player {
 		})
 	}
 	return players
+}
+
+func (s *scrabblews) sendError(data *ReceiverData, message string, err error) {
+	fmt.Println(fmt.Sprintf("ERROR Data: %s Dump: %s", data, err.Error()))
+	msg := clientMessage{
+		Body: message,
+		Type: "ERROR",
+	}
+	b, _ := json.Marshal(msg)
+	Send(&Message{
+		Domain:  data.Domain,
+		Stage:   data.Stage,
+		Message: string(b),
+	})
 }
