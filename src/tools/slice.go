941cf7e4747568bac2492ce1279648f2b5b8ca18
YouJinTou <dandreevd@gmail.com>
2020-11-15 17:14:14 +0200

    Changed string.go to slice.go.

--- src/tools/slice.go
+++ src/tools/slice.go
@@ -9,3 +9,13 @@ func ContainsString(s []string, t string) bool {
 	}
 	return false
 }
+
+// ContainsInt checks if a slice of ints contains a given int.
+func ContainsInt(s []int, t int) bool {
+	for _, i := range s {
+		if i == t {
+			return true
+		}
+	}
+	return false
+}
