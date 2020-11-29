8170ae9a3f8fd78fa3dc5b271280f43b77122124
YouJinTou <dandreevd@gmail.com>
2020-11-29 10:54:47 +0200

    Added a games overview screen.

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlanksDialog, ScrabbleComponent } from './components/scrabble/scrabble.component';
 import { WebsocketService } from 'src/services/websocket.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GamesOverviewComponent } from './components/games-overview/games-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScrabbleComponent,
-    BlanksDialog
+    BlanksDialog,
+    GamesOverviewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    MatSelectModule,
+    MatDividerModule,
   ],
   providers: [WebsocketService],
   bootstrap: [AppComponent]
