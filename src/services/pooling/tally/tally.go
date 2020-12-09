c63c98a9a2d8fdce49595ad00406f26a0ede5aba
YouJinTou <dandreevd@gmail.com>
2020-12-10 00:06:38 +0200

    Refactored to pure functions.

--- src/services/pooling/tally/tally.go
+++ src/services/pooling/tally/tally.go
@@ -23,60 +23,84 @@ func handle(ctx context.Context, e events.DynamoDBEvent) error {
 	sess := session.Must(session.NewSession())
 	dynamo := dynamodb.New(sess)
 	for _, r := range e.Records {
-		if err := tally(dynamo.GetItem, dynamo.UpdateItem, r); err != nil {
+		division, joined, players := division(r.Change)
+		d, _ := tools.GetItem(tools.Table("tallies"), "ID", division, nil, nil, nil)
+		shouldPool := willReachCapacity(d, joined)
+		var i *dynamodb.UpdateItemInput
+
+		if shouldPool {
+			i = poolInput(division, r.Change.NewImage["ID"].String())
+		} else if joined {
+			i = connectInput(division, r.Change.NewImage["ID"].String(), players)
+		} else {
+			i = disconnectInput(division, r.Change.OldImage["ID"].String())
+		}
+		if _, err := dynamo.UpdateItem(i); err != nil {
 			log.Print(err)
 		}
 	}
 	return nil
 }
 
-func tally(
-	getItem func(*dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error),
-	updateItem func(*dynamodb.UpdateItemInput) (*dynamodb.UpdateItemOutput, error),
-	r events.DynamoDBEventRecord) error {
-	division, sign, players := division(r.Change)
-	shouldPool := willReachCapacity(division, sign, getItem)
-	var ue, w string
-	if shouldPool {
-		w = "0"
-		ue = fmt.Sprintf("SET Waiting = :w + :z, #c = :c, ShouldPool = :s")
-	} else {
-		w = "1"
-		ue = fmt.Sprintf(
-			"SET Waiting = if_not_exists(Waiting, :z) %s :w, #c = :c, ShouldPool = :s", sign)
-	}
-	fmt.Println(ue)
-	_, err := updateItem(&dynamodb.UpdateItemInput{
+func poolInput(division, connectionID string) *dynamodb.UpdateItemInput {
+	ue := "REMOVE ConnectionIDs SET LastConnectionID = :cid, ShouldPool = :s"
+	i := &dynamodb.UpdateItemInput{
 		TableName:        tools.Table("tallies"),
 		Key:              map[string]*dynamodb.AttributeValue{"ID": {S: aws.String(division)}},
 		UpdateExpression: aws.String(ue),
 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":w": &dynamodb.AttributeValue{N: aws.String(w)},
-			":c": &dynamodb.AttributeValue{N: aws.String(strconv.Itoa(players))},
-			":z": &dynamodb.AttributeValue{N: aws.String("0")},
-			":s": &dynamodb.AttributeValue{BOOL: aws.Bool(shouldPool)},
+			":cid": &dynamodb.AttributeValue{S: aws.String(connectionID)},
+			":s":   &dynamodb.AttributeValue{BOOL: aws.Bool(true)},
 		},
-		ExpressionAttributeNames: map[string]*string{"#c": aws.String("Capacity")},
-	})
-	return err
+	}
+	return i
 }
 
-func division(r events.DynamoDBStreamRecord) (string, string, int) {
+func connectInput(division, connectionID string, players int) *dynamodb.UpdateItemInput {
+	ue := fmt.Sprintf("%s %s, %s", "ADD ConnectionIDs :c", "SET ShouldPool = :s", "#c = :cap")
+	i := &dynamodb.UpdateItemInput{
+		TableName:        tools.Table("tallies"),
+		Key:              map[string]*dynamodb.AttributeValue{"ID": {S: aws.String(division)}},
+		UpdateExpression: aws.String(ue),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":c":   &dynamodb.AttributeValue{SS: []*string{aws.String(connectionID)}},
+			":cap": &dynamodb.AttributeValue{N: aws.String(strconv.Itoa(players))},
+			":s":   &dynamodb.AttributeValue{BOOL: aws.Bool(false)},
+		},
+		ExpressionAttributeNames: map[string]*string{"#c": aws.String("Capacity")},
+	}
+	return i
+}
+
+func disconnectInput(division, connectionID string) *dynamodb.UpdateItemInput {
+	ue := fmt.Sprintf("%s %s", "DELETE ConnectionIDs :c", "SET ShouldPool = :s")
+	i := &dynamodb.UpdateItemInput{
+		TableName:        tools.Table("tallies"),
+		Key:              map[string]*dynamodb.AttributeValue{"ID": {S: aws.String(division)}},
+		UpdateExpression: aws.String(ue),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":c":   &dynamodb.AttributeValue{SS: []*string{aws.String(connectionID)}},
+			":s":   &dynamodb.AttributeValue{BOOL: aws.Bool(false)},
+			":cid": &dynamodb.AttributeValue{S: aws.String(connectionID)},
+		},
+		ConditionExpression: aws.String("contains(ConnectionIDs, :cid)"),
+	}
+	return i
+}
+
+func division(r events.DynamoDBStreamRecord) (string, bool, int) {
 	_, playerJoined := r.NewImage["LiveUntil"]
 	var bucket, game, language string
 	var players int
-	var sign string
 
 	if playerJoined {
 		bucket, game, language, players = extract(r.NewImage)
-		sign = "+"
 	} else {
 		bucket, game, language, players = extract(r.OldImage)
-		sign = "-"
 	}
 
 	division := fmt.Sprintf("%s_%s_%s_%s_%d", os.Getenv("STAGE"), game, bucket, language, players)
-	return division, sign, players
+	return division, playerJoined, players
 }
 
 func extract(m map[string]events.DynamoDBAttributeValue) (string, string, string, int) {
@@ -84,25 +108,15 @@ func extract(m map[string]events.DynamoDBAttributeValue) (string, string, string
 	return m["Bucket"].String(), m["Game"].String(), m["Language"].String(), int(players)
 }
 
-func willReachCapacity(
-	division, sign string,
-	getItem func(*dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error)) bool {
-	if sign == "-" {
+func willReachCapacity(d *dynamodb.GetItemOutput, joined bool) bool {
+	if d.Item == nil || !joined {
 		return false
 	}
 
-	d, _ := getItem(&dynamodb.GetItemInput{
-		TableName: tools.Table("tallies"),
-		Key: map[string]*dynamodb.AttributeValue{
-			"ID": &dynamodb.AttributeValue{S: aws.String(division)},
-		},
-	})
-
-	if d.Item == nil {
-		return false
+	var waiting = 0
+	if val, ok := d.Item["ConnectionIDs"]; ok {
+		waiting = len(val.SS)
 	}
-
-	waiting, _ := strconv.Atoi(*d.Item["Waiting"].N)
 	capacity, _ := strconv.Atoi(*d.Item["Capacity"].N)
 	return waiting+1 >= capacity
 }
