3acfab8ac789db94ae64dfecf5f3c2cbea9155f3
YouJinTou <dandreevd@gmail.com>
2020-12-20 14:42:21 +0200

    Removed OnDestroy.

--- ui/src/app/components/wordlines/wordlines.component.ts
+++ ui/src/app/components/wordlines/wordlines.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment';
 import { ContextService } from 'src/services/context.service';
