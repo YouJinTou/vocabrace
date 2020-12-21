2ff08dd948adc6d11de12bcbde160ea46c14cdfb
YouJinTou <dandreevd@gmail.com>
2020-12-21 09:53:50 +0200

    Removed 'back to game' button OnGameOver().

--- ui/src/services/game-over.service.ts
+++ ui/src/services/game-over.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { ContextService } from './context.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameOverService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private contextService: ContextService) { }
 
   onGameOver(payload) {
+    this.contextService.setIsPlaying({ value: false, pid: '' });
     // this.httpClient.post(environment.gameOverEndpoint, payload);
   }
 }
