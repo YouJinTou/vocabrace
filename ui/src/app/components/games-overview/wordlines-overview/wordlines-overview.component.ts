793fc655226a4fd2c10c5e3e959aecc540102e92
YouJinTou <dandreevd@gmail.com>
2020-12-21 17:28:00 +0200

    Removed instastartgame.

--- ui/src/app/components/games-overview/wordlines-overview/wordlines-overview.component.ts
+++ ui/src/app/components/games-overview/wordlines-overview/wordlines-overview.component.ts
@@ -1,59 +1,4 @@
-// import { Component, OnDestroy, OnInit } from '@angular/core';
-
-// import { Router } from '@angular/router';
-// import { environment } from 'src/environments/environment';
-// import { ContextService } from 'src/services/context.service';
-// import { WebsocketService } from 'src/services/websocket.service';
-
-// @Component({
-//   selector: 'app-wordlines-overview',
-//   templateUrl: './wordlines-overview.component.html',
-//   styleUrls: ['./wordlines-overview.component.css']
-// })
-// export class WordlinesOverviewComponent implements OnInit {
-//   selectedPlayers: string;
-//   selectedLanguage: string;
-
-//   constructor(
-//     private wsService: WebsocketService,
-//     private contextService: ContextService,
-//     private router: Router) { }
-
-//   ngOnInit(): void {
-//   }
-
-//   onSelectChanged() {
-//     this.connect();
-//   }
-
-//   private connect() {
-//     if (!(this.selectedLanguage && this.selectedPlayers)) {
-//       return;
-//     }
-    
-//     this.wsService.connect(environment.wsEndpoint, {
-//       'game': 'wordlines',
-//       'players': parseInt(this.selectedPlayers),
-//       'language': this.selectedLanguage,
-//       'userID': this.contextService.user.id,
-//     }).subscribe({
-//       next: m => {
-//         if ('pid' in m) {
-//           this.contextService.setWordlines({
-//             players: parseInt(this.selectedPlayers),
-//             userId: this.contextService.user.id,
-//             language: this.selectedLanguage,
-//             poolId: m['pid']
-//           });
-//           this.contextService.setIsPlaying(true);
-//           this.router.navigate(['wordlines', m['pid']]);
-//         }
-//       },
-//       error: e => console.log(e)
-//     });
-//   }
-// }
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
@@ -75,7 +20,6 @@ export class WordlinesOverviewComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit(): void {
-    this.connect();
   }
 
   onSelectChanged() {
@@ -83,20 +27,24 @@ export class WordlinesOverviewComponent implements OnInit {
   }
 
   private connect() {
+    if (!(this.selectedLanguage && this.selectedPlayers)) {
+      return;
+    }
+
     this.wsService.connect(environment.wsEndpoint, {
       'game': 'wordlines',
-      'players': 2,
-      'language': 'english',
+      'players': parseInt(this.selectedPlayers),
+      'language': this.selectedLanguage,
       'userID': this.contextService.user.id,
     }).subscribe({
       next: m => {
         if ('pid' in m) {
           this.contextService.setWordlines({
-            players: 2,
-            language: 'english',
+            players: parseInt(this.selectedPlayers),
+            language: this.selectedLanguage,
             poolId: m['pid']
           });
-          this.contextService.setIsPlaying({ value: true, pid: m['pid']});
+          this.contextService.setIsPlaying({ value: true, pid: m['pid'] });
           this.router.navigate(['wordlines', m['pid']]);
         }
       },
